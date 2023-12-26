4f43a715299fe1b5782640d04b94fe1bcb926faf
kkard2 <kkard2.acct@gmail.com>
2023-12-26 12:31:53 +0100

    at last

--- nvim/init.lua
+++ nvim/init.lua
@@ -200,4 +200,27 @@ require("lazy").setup({
     { "hrsh7th/nvim-cmp" },
     { "L3MON4D3/LuaSnip" },
     { "neovim/nvim-lspconfig" },
+
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+            vim.keymap.set("n", "<leader>s", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+            vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+            vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
+            vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
+        end,
+    }
 })
