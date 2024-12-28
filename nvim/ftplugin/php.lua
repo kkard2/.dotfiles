ca8e5b05498f6bf61542290742fe25aa91af40e2
kkard2 <contact@kkard2.com>
2024-12-28 12:19:57 +0100

    make highlight less visible

--- nvim/ftplugin/php.lua
+++ nvim/ftplugin/php.lua
@@ -1,9 +1,9 @@
 local copy_indent_macro = "<Esc>0\"_D" -- clear current line
     .. "?.<CR>" -- find first non-empty line above
+    .. "<cmd>noh<CR>" -- clear highlight
     .. "^\"yy0" -- copy indentation to y register
     .. "<C-o>" -- return to original line
     .. "\"yp" -- paste y register
-    .. "<cmd>noh<CR>" -- clear highlight
     .. "A" -- enter insert mode
 
 vim.keymap.set({ "n" }, "o", "o" .. copy_indent_macro, { buffer = 0 })
